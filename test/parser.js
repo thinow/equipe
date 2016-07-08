7eac66b53518213f09d779d10033b2a299e1e5e7
Thierry Nowak <thinow@gmail.com>
2016-07-08 23:09:03 +0200

    parser: read XLSX

--- test/parser.js
+++ test/parser.js
@@ -1,17 +1,38 @@
 var expect = require("chai").expect;
 var parser = require("../lib/parser");
+var fs = require('fs');
 
 describe("Excel files parser", function () {
 
-    it("Cannot parse undefined", function () {
-        expect(function () {
-            parser.parse(undefined)
-        }).to.throw(ReferenceError);
+    function readTestFile() {
+        return fs.readFileSync(__dirname + '/resources/test-file.xlsx');
+    }
+
+    describe("Integration tests", function () {
+
+        it("Can parse test file", function () {
+            // when
+            var result = parser.parse(readTestFile());
+
+            // then
+            expect(result).to.exist;
+        });
     });
 
-    it("Can parse data", function () {
-        expect(function () {
-            parser.parse('blabla');
-        }).to.not.throw(Error);
+    describe("Unit tests", function () {
+
+        it("Cannot parse undefined", function () {
+            expect(function () {
+                parser.parse(undefined)
+            }).to.throw(ReferenceError);
+        });
+
+        it("Read as an Excel file", function () {
+            // when
+            var result = parser.readAsXlsx(readTestFile());
+
+            // then
+            expect(result).to.have.deep.property('SheetNames[0]', 'Sheet1');
+        });
     });
 });
